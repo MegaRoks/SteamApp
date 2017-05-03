c8c8acb35a7bc4cf54c68022403d33c473601a95
Maksim <MegaRoks@gmail.com>
2017-05-03 12:59:42 +0600

    Dobabotal search on ides, has begun to do information on the status

--- ParserSteam/Controllers/HomeController.cs
+++ ParserSteam/Controllers/HomeController.cs
@@ -22,9 +22,15 @@ namespace ParserSteam.Controllers
             ViewBag.steamID64 = xdoc.Element("profile").Element("steamID64").Value;
             ViewBag.steamID = xdoc.Element("profile").Element("steamID").Value;
             ViewBag.onlineState = xdoc.Element("profile").Element("onlineState").Value;
-            ViewBag.avatarIcon = xdoc.Element("profile").Element("avatarIcon").Value;
+            ViewBag.stateMessage = xdoc.Element("profile").Element("stateMessage").Value;
+            ViewBag.privacyState = xdoc.Element("profile").Element("privacyState").Value;
             ViewBag.memberSince = xdoc.Element("profile").Element("memberSince").Value;
             ViewBag.location = xdoc.Element("profile").Element("location").Value;
+            ViewBag.avatarFull = xdoc.Element("profile").Element("avatarFull").Value;
+            ViewBag.vacBanned = xdoc.Element("profile").Element("vacBanned").Value;
+            ViewBag.tradeBanState = xdoc.Element("profile").Element("tradeBanState").Value;
+            ViewBag.isLimitedAccount = xdoc.Element("profile").Element("isLimitedAccount").Value;
+            ViewBag.summary = xdoc.Element("profile").Element("summary").Value;
             return View();
         }
     }
