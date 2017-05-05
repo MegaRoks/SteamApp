f8df2bcb154f0e0f768911fbcd8d0a0805ee89f3
Maksim <MegaRoks@gmail.com>
2017-05-05 16:41:51 +0600

    I have finished search

--- ParserSteam/Controllers/HomeController.cs
+++ ParserSteam/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using ParserSteam.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace ParserSteam.Controllers
 {
@@ -18,24 +20,25 @@ namespace ParserSteam.Controllers
         [HttpGet]
         public ActionResult Info(string steamIDS)
         {
-            XDocument xdoc = XDocument.Load("http://steamcommunity.com/profiles/" + steamIDS + "/?xml=1");
-            ViewBag.steamID64 = xdoc.Element("profile").Element("steamID64").Value;
-            ViewBag.steamID = xdoc.Element("profile").Element("steamID").Value;
-            ViewBag.onlineState = xdoc.Element("profile").Element("onlineState").Value;
-            ViewBag.stateMessage = xdoc.Element("profile").Element("stateMessage").Value;
-            ViewBag.privacyState = xdoc.Element("profile").Element("privacyState").Value;
-            ViewBag.memberSince = xdoc.Element("profile").Element("memberSince").Value;
-            ViewBag.location = xdoc.Element("profile").Element("location").Value;
-            ViewBag.avatarFull = xdoc.Element("profile").Element("avatarFull").Value;
-            ViewBag.vacBanned = xdoc.Element("profile").Element("vacBanned").Value;
-            ViewBag.tradeBanState = xdoc.Element("profile").Element("tradeBanState").Value;
-            ViewBag.isLimitedAccount = xdoc.Element("profile").Element("isLimitedAccount").Value;
-            ViewBag.summary = xdoc.Element("profile").Element("summary").Value;
-            ViewBag.gameLink = xdoc.Element("profile").Element("mostPlayedGames").Element("mostPlayedGame").Element("gameLink").Value;
-            ViewBag.gameLogo = xdoc.Element("profile").Element("mostPlayedGames").Element("mostPlayedGame").Element("gameLogo").Value;
-            ViewBag.hoursPlayed = xdoc.Element("profile").Element("mostPlayedGames").Element("mostPlayedGame").Element("hoursPlayed").Value;
-            ViewBag.hoursOnRecord = xdoc.Element("profile").Element("mostPlayedGames").Element("mostPlayedGame").Element("hoursOnRecord").Value;
-            return View();
+            long d;
+            if (long.TryParse(steamIDS, out d))
+            {
+                XDocument xdoc = XDocument.Load("http://steamcommunity.com/profiles/" + steamIDS + "/?xml=1");
+                var q = FromXml(xdoc);
+                return View(q);
+            }
+            else
+            {
+                XDocument xdoc = XDocument.Load("http://steamcommunity.com/id/" + steamIDS + "/?xml=1");
+                var q = FromXml(xdoc);
+                return View(q);
+            }             
+        }
+
+        private Profile FromXml(XDocument xd)
+        {
+            XmlSerializer s = new XmlSerializer(typeof(Profile));
+            return (Profile)s.Deserialize(xd.CreateReader());
         }
     }
 }
