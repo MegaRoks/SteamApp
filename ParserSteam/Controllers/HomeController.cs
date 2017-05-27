b2d2d6b1c84fb5d5cfb460a540f8c7dae81c2500
Maksim <MegaRoks@gmail.com>
2017-05-27 21:07:35 +0600

    final version

--- ParserSteam/Controllers/HomeController.cs
+++ ParserSteam/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ using System.Xml.Serialization;
 namespace ParserSteam.Controllers
 {
     public class HomeController : Controller
-    {       
+    {
         [HttpGet]
         public ActionResult Index()
         {
@@ -21,24 +21,54 @@ namespace ParserSteam.Controllers
         public ActionResult Info(string steamIDS)
         {
             long d;
+            XDocument xdoc;
+            Profile result;
             if (long.TryParse(steamIDS, out d))
             {
-                XDocument xdoc = XDocument.Load("http://steamcommunity.com/profiles/" + steamIDS + "/?xml=1");
-                var q = FromXml(xdoc);
-                return View(q);
+                xdoc = XDocument.Load("http://steamcommunity.com/profiles/" + steamIDS + "/?xml=1");
             }
             else
             {
-                XDocument xdoc = XDocument.Load("http://steamcommunity.com/id/" + steamIDS + "/?xml=1");
-                var q = FromXml(xdoc);
-                return View(q);
-            }             
+                xdoc = XDocument.Load("http://steamcommunity.com/id/" + steamIDS + "/?xml=1");
+            }
+            try
+            {
+                result = FromXml(xdoc);
+                if (result.Groups != null)
+                {
+                    if (result.Groups.Group.Count > 3)
+                    {
+                        FillGroups(result);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                result = null;
+                ViewBag.error = ex.Message;
+            }
+            
+            return View(result);
         }
 
         private Profile FromXml(XDocument xd)
         {
             XmlSerializer s = new XmlSerializer(typeof(Profile));
             return (Profile)s.Deserialize(xd.CreateReader());
+        }
+
+        private void FillGroups(Profile profile)
+        {
+            XmlSerializer s = new XmlSerializer(typeof(MemberList));
+            if (profile.Groups.Group.Count > 3)
+            {
+                for (int i = 3; i < profile.Groups.Group.Count; i++)
+                {
+                    string groupID64 = profile.Groups.Group[i].GroupID64;
+                    XDocument xdoc = XDocument.Load("http://steamcommunity.com/gid/" + groupID64 + "/memberslistxml/?xml=1");
+                    profile.Groups.Group[i] = ((MemberList)s.Deserialize(xdoc.CreateReader())).GroupDetails;
+                }
+            }
         }
     }
 }
